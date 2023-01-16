1634d20467179c5b6062193629af7e6ae6ad3b26
Germán Zittermann <germanzittermann@gmail.com>
2023-01-16 03:39:01 -0300

    Generation of random numbers fixed

--- responses.py
+++ responses.py
@@ -3,7 +3,7 @@ import random
 def handle_response(message) -> str:
 
 	p_message = message.lower()
-	random_number = str(random.randint(0, 100))
+	random_number = str(random.randint(0, 99))
 
 	if p_message == 'dado':
 		return f':game_die: = {random_number}'
