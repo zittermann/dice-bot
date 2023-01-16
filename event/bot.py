5bad990ea95146a193d04ad31110b3f295c889f3
Germán Zittermann <germanzittermann@gmail.com>
2023-01-16 17:40:06 -0300

    Project refactored

---
+++ event/bot.py
@@ -0,0 +1,40 @@
+import discord, os
+from event import responses
+from dotenv import load_dotenv
+
+load_dotenv()
+
+async def send_message(message, user_message):
+	
+	try:
+
+		response = responses.handle_response(user_message)
+
+		await message.reply(response)
+
+	except Exception as e:
+		print(e)
+
+
+def run_bot():
+
+	intents = discord.Intents.all()
+	client = discord.Client(intents=intents)
+
+	@client.event
+	async def on_ready():
+		print(f"{client.user} is now running!")
+
+	@client.event
+	async def on_message(message):
+
+		if message.author == client.user:
+			return  
+
+		user_message = str(message.content)
+		
+		if '!' in user_message:
+			# user_message = user_message[1:]
+			await send_message(message, user_message)
+
+	client.run(os.getenv('TOKEN'))
