3f9bd50e4b553d21f0f1ccb3e90ffbbb3c3d3197
Germán Zittermann <germanzittermann@gmail.com>
2023-01-16 09:50:33 -0300

    doubles added

--- responses.py
+++ responses.py
@@ -2,8 +2,13 @@ import random
 
 def handle_response(message) -> str:
 
-	p_message = message.lower()
-	random_number = str(random.randint(0, 99))
+	p_message = message.lower().split(' ')
 
-	if p_message == 'dado':
+	if p_message[0] == 'dado':
+		random_number = str(random.randint(0, 99))
 		return f':game_die: = {random_number}'
+
+	if p_message[0] == 'dobles':
+		first_random = str(random.randint(0, 99))
+		second_random = str(random.randint(0, 99))
+		return f':game_die: = {first_random}/{second_random}'
