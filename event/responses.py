2b8c5ef97d28a409367e479d0d5c059c02070460
Germán Zittermann <germanzittermann@gmail.com>
2023-01-16 17:51:00 -0300

    Bot always return a two digits number

--- event/responses.py
+++ event/responses.py
@@ -1,14 +1,21 @@
 import random
 
+def generate_number() -> str:
+	
+	random_number = str(random.randint(0, 99))
+
+	if len(random_number) == 1:
+		return f'0{random_number}' 
+
+	return random_number
+
+
 def handle_response(message) -> str:
 
 	p_message = message.lower().split(' ')
 
 	if '!dado' in p_message or '!reroll' in p_message:
-		random_number = str(random.randint(0, 99))
-		return f':game_die: = {random_number}'
+		return f':game_die: = {generate_number()}'
 
 	if '!dobles' in p_message:
-		first_random = str(random.randint(0, 99))
-		second_random = str(random.randint(0, 99))
-		return f':game_die: = {first_random}/{second_random}'
+		return f':game_die: = {generate_number()}/{generate_number()}'
