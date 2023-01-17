f65deda854da76d0f9abeef04a22607d65e1ac79
Germán Zittermann <germanzittermann@gmail.com>
2023-01-16 23:17:26 -0300

    Script running added to main

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from event import bot
+import script
 from database.connection import create_connection
 
+DB = create_connection()
+
 if __name__ == '__main__':
+	
 	bot.run_bot()
+	# script.run_script(DB)
