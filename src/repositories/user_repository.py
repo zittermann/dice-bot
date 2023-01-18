df58daca840fcc5859b807a6cb40936433109bc0
Germán Zittermann <germanzittermann@gmail.com>
2023-01-18 18:38:33 -0300

    User repository created

---
+++ src/repositories/user_repository.py
@@ -0,0 +1,29 @@
+
+def find(db, username):
+	collection = db.users
+	return collection.find_one({
+		"username": username
+	})
+
+def create(db, username):
+
+	collection = db.users
+	total_users = collection.count_documents({})
+
+	new_user = {
+		"_id": total_users,
+		"username": username,
+		"latest": None,
+		"record": 0,
+		"consecutively": 0
+	}
+
+	collection.insert_one(new_user)
+
+def update(db, updated_user):
+
+	collection = db.users
+
+	collection.find_one_and_update({
+		"_id": updated_user["_id"]
+	}, updated_user)	
