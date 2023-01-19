aa90bda50d04673d3c75fcf2a7a1b09b88c16564
Germán Zittermann <germanzittermann@gmail.com>
2023-01-18 23:01:30 -0300

    Now method added to simplify logic. Number to emote tool added too

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,6 @@
 import random
 from datetime import datetime, timedelta
+from assets import emotes as e
 
 def generate_number() -> str:
 	
@@ -10,8 +11,26 @@ def generate_number() -> str:
 
 	return random_number
 
-def today() -> datetime:
+
+# the difference between now and today is that 
+# today is converted to string  
+def now() -> datetime:
 	return datetime.now()
 
+def today() -> str:
+	return now().strftime('%Y-%m-%d')
+
+# Use now instead of today so we can use math logic
+# to generate the date of yesterday
 def yesterday() -> str:
-	return today() - timedelta(1).strftime('%Y-%m-%d')
+	return (now() - timedelta(1)).strftime('%Y-%m-%d')
+
+# turn a number into its equivalents with discord emotes
+def number_to_emote(number):
+
+	emote = ""
+
+	for i in iter(str(number)): # iter(str(number)) converts number to integer
+		emote += e.NUMBERS[int(i)] # convert index to integer again so we can find it's emoji equivalent
+
+	return emote
