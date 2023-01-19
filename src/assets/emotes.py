978b98c4b9dc03b2aeb9729727e01fa6a2aa0b8f
Germán Zittermann <germanzittermann@gmail.com>
2023-01-18 22:58:30 -0300

    New emotes added

--- src/assets/emotes.py
+++ src/assets/emotes.py
@@ -5,3 +5,12 @@ SMALL_DICK = ':smalldick:'
 RAGE = ':rage~1:'
 HONRY = ':osopedroso'
 KEK = ':kek:'
+NO_IDEA = ':noidea:'
+PEKO = ':peko:'
+JUDEN = ':juden:'
+COOMER = ':coomer:'
+OK_ZOOMER = ':okzoomer:'
+SKULL = ':skull:'
+
+NUMBERS = [':zero:', ':one:', ':two:', ':three:', ':four:',
+	':five:', ':six:', ':seven:', ':eight:', ':nine:']
