c9df34851f7f1556b0c5c29a115d5e4fd2930ccf
Germán Zittermann <germanzittermann@gmail.com>
2023-01-18 23:01:50 -0300

    why am i even doing this...

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from events import bot
-import src.scripts.script as script
+# import scripts.script
 from database.connection import create_connection
 
-DB = create_connection()
+from utils import utils
 
 if __name__ == '__main__':
-	
+
 	bot.run_bot()
 	# script.run_script(DB)
